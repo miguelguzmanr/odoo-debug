c3fa7f18082bcd8f0bffe231209a1491acae1e72
Miguel Guzmán <40463442+miguelguzmanr@users.noreply.github.com>
2021-09-12 20:28:30 -0400

    Create __manifest__.py

---
+++ debug_debugpy/__manifest__.py
@@ -0,0 +1,17 @@
+
+{
+    "name": "Debugpy",
+    "summary": "A debugger for python",
+    "version": "14.0.1.0",
+    "category": "Hidden",
+    "author": "Miguel A. Guzmán Rodríguez <miguelguzmanr@outlook.com>",
+    "website": "https://github.com/miguelguzmanr/odoo-debug",
+    "license": "LGPL-3",
+    "depends": [
+    ],
+    "data": [
+    ],
+    "installable": True,
+    "autoinstall": False,
+    "application": False,
+}
